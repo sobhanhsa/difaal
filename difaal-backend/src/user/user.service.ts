0c4fce3116325f5f48c5bbd4fd232d6dc8c4444d
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 17:59:26 +0330

    feat : use prisma

--- difaal-backend/src/user/user.service.ts
+++ difaal-backend/src/user/user.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
+import { DataBaseService } from 'src/database/database.service';
 
 @Injectable()
 export class UserService {
+    constructor(private prisma:DataBaseService){}
+
     showProfileHandler(request: Request) {
         return request.user
     }
