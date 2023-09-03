7b7c75480812d0c725acbfda8a804bdea531fbf1
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 21:07:36 +0330

    feat : config publish dto

--- difaal-backend/src/advertisement/dto/publish-ad.dto.ts
+++ difaal-backend/src/advertisement/dto/publish-ad.dto.ts
@@ -1,11 +1,21 @@
+import { IsArray, IsNotEmpty, IsString, MaxLength } from "class-validator"
+
 export class publishAdDto {
     
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(16)
     title:string
     
+    @IsArray()
     photos:string
     
+    @IsString()
+    @IsNotEmpty()
     description:string
 
+    @IsString()
+    @IsNotEmpty()
     category:string
 
 }
