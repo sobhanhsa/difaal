18874fc1cb9c4c4f68e9bd0fc3f449a724724c48
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 10:33:30 +0330

    refactor : signin dto exportation

--- difaal-backend/src/auth/dto/signin.dto.ts
+++ difaal-backend/src/auth/dto/signin.dto.ts
@@ -1,6 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
-export class signUpDto {
+export class signInDto {
     @IsEmail()
     @IsNotEmpty()
     email:string
