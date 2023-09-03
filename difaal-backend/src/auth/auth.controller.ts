14bd7836fc8de8a7a09c68f7b8769723d93c8290
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 09:00:46 +0330

    feat : add sign up root handler

--- difaal-backend/src/auth/auth.controller.ts
+++ difaal-backend/src/auth/auth.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { signUpDto } from "./dto";
 
 @Controller()
 export class AuthController {
+    
+    @HttpCode(201)
+    @Post('signup')
+    signUp (@Body() dto:signUpDto) {
+
+    }
 
 }
