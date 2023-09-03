ac7e0fc23244bb4dde7fe460f1c86f140c5df268
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 09:20:43 +0330

    feat : use cookie parser

--- difaal-backend/src/main.ts
+++ difaal-backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,5 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
       whitelist:true
   }))
+  app.use(cookieParser())
 }
 bootstrap();
