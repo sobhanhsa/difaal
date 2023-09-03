b54b27ded10bb9f87c1a422483be364b63649713
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 17:47:22 +0330

    feat : doploy and handle user/profile root

--- difaal-backend/src/user/user.controller.ts
+++ difaal-backend/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { UserService } from './user.service';
+import { CookieJwtGuard } from 'src/auth/guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private userService:UserService){}
+
+    @UseGuards(CookieJwtGuard)
+    @Get('profile')
+    showUserProfile(@Req() req:Request) {
+        return this.userService.showProfileHandler(req);
+    }
+
+    
+
+}
--- difaal-backend/src/user/user.service.ts
+++ difaal-backend/src/user/user.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    showProfileHandler(request: Request) {
+        return request.user
+    }
+}
