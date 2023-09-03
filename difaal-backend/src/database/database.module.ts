238b2ac0a6c90f79cdbe9840754351c276f47340
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 09:07:37 +0330

    perf : make databse module global

--- difaal-backend/src/database/database.module.ts
+++ difaal-backend/src/database/database.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DataBaseService } from './database.service';
 
+@Global()
 @Module({
     providers:[DataBaseService],
     exports:[DataBaseService]
