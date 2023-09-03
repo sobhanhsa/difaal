001f9bcf56828130c543d1171540648dffecaaff
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 10:31:48 +0330

    feat : create signin dto

---
+++ difaal-backend/src/auth/dto/signin.dto.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class signUpDto {
+    @IsEmail()
+    @IsNotEmpty()
+    email:string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8)
+    password:string
+
+}
