78b709f003d3944fca26d95e51540ea39c7e062c
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 18:06:31 +0330

    feat : setup update user route handler

--- difaal-backend/src/user/user.service.ts
+++ difaal-backend/src/user/user.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
 import { Request } from 'express';
 import { DataBaseService } from 'src/database/database.service';
+import { updateUserDto } from './dto';
 
 @Injectable()
 export class UserService {
@@ -9,4 +11,30 @@ export class UserService {
     showProfileHandler(request: Request) {
         return request.user
     }
+
+    async updateUserProfile( { id } , dto : updateUserDto) {
+
+        try {
+            const updatedUser = await this.prisma.user.update({
+            where: {
+                id:id,
+            },
+            data: {
+                ...dto
+            }})
+
+            return updateUserDto
+
+        } catch(e) {
+            if ((e instanceof Prisma.PrismaClientKnownRequestError) && (e.code === "P2002")) {            
+                throw new ForbiddenException(
+                        "taken email",
+                    );
+            }
+            
+            throw e
+        }
+
+    }
+
 }
