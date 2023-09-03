ce217f297f8037e706b891228a021cd38ef735a3
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 18:22:14 +0330

    feat : make dto optional

--- difaal-backend/src/user/dto/update-user.dto.ts
+++ difaal-backend/src/user/dto/update-user.dto.ts
@@ -1,11 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class updateUserDto {
     
     @IsString()
     @IsNotEmpty()
+    @IsOptional()
     name:string
 
+    @IsOptional()
     @IsEmail()
     @IsNotEmpty()
     email:string    
--- difaal-backend/src/user/user.controller.ts
+++ difaal-backend/src/user/user.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { UserService } from './user.service';
 import { CookieJwtGuard } from 'src/auth/guard';
+import { updateUserDto } from './dto';
+import { UserParamDecorator } from './decorator';
 
 @Controller('user')
 export class UserController {
     constructor(private userService:UserService){}
 
     @UseGuards(CookieJwtGuard)
-    @Get('profile')
+    @Get('me')
     showUserProfile(@Req() req:Request) {
         return this.userService.showProfileHandler(req);
     }
 
-    
+    @UseGuards(CookieJwtGuard)    
+    @Patch('me')
+    updataUserInfo(
+        @UserParamDecorator("id") userId:number,
+        @Body() dto     : updateUserDto
+    ) {
+        this.userService.updateUserProfile({id:userId},dto)
+    }
+
+
+
+
+
 
 }
