3e17efedbd3d7eb90a5d5e72be1e080dd3ecb3d5
sobhanhsa <sobhanb7b7@protonmail.com>
2023-09-03 17:27:09 +0330

    feat : add user module

--- difaal-backend/src/app.module.ts
+++ difaal-backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule , ConfigModule.forRoot({ isGlobal: true}),AuthModule],
+  imports: [DatabaseModule , ConfigModule.forRoot({ isGlobal: true}),AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
